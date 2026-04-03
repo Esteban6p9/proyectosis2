d118102464a52015a0b6ebb5d70f50782f9d7f08
andreanicolecadimarodriguez-cr <andreanicolecadimarodriguez@gmail.com>
2026-04-02 23:08:06 -0400

    commit base de datos

---
+++ nicol/app/api/items/[id]/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import pool from '@/lib/db'; 
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+  try {
+   
+    const id = params.id;
+
+    
+    const [result] = await pool.query('DELETE FROM pintura_arte WHERE id = ?', [id]);
+
+   
+    return NextResponse.json({ success: true, message: 'Técnica eliminada correctamente' });
+    
+  } catch (error) {
+    console.error("Error al eliminar:", error);
+    return NextResponse.json({ error: 'Error al intentar eliminar el registro.' }, { status: 500 });
+  }
+}
